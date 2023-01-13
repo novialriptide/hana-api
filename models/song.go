53559aabf9241824fdefddd5a63bbd7c0d78b266
Andrew Hong <andrewhong@myyahoo.com>
2023-01-12 22:59:36 -0500

    songsourceid -> songsource

--- models/song.go
+++ models/song.go
@@ -3,12 +3,12 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Song struct {
-	ID           primitive.ObjectID `bson:"_id"`
-	SongID       string             `bson:"song_id"`
-	AlbumID      string             `bson:"album_id"`
-	ArtistIDs    []string           `bson:"artist_ids"`
-	SongGenreID  string             `bson:"song_genre_id"`
-	SongSourceID string             `bson:"source_id"`
-	SongName     string             `bson:"song_name"`
-	SongLength   int32              `bson:"song_length"`
+	ID          primitive.ObjectID `bson:"_id"`
+	SongID      string             `bson:"song_id"`
+	AlbumID     string             `bson:"album_id"`
+	ArtistIDs   []string           `bson:"artist_ids"`
+	SongGenreID string             `bson:"song_genre_id"`
+	SongSource  string             `bson:"song_source"`
+	SongName    string             `bson:"song_name"`
+	SongLength  int32              `bson:"song_length"`
 }
--- models/source.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Source struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	SourceID   string             `bson:"source_id"`
-	SourceName string             `bson:"source_name"`
-}
